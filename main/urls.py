f3675232d44b2fe3872f66295054119b2f8ada96
ano <christianoimannuel@gmail.com>
2026-09-09 23:49:19 +0700

    Merapihkan isi CSS dan juga mengisi instance untuk experience

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from main.views import show_main, show_experience, show_experience_detail
+
+app_name = "main"
+
+urlpatterns = [
+    path("", show_main, name="show_main"),
+    path("experience/", show_experience, name="show_experience"),
+    path("experience/<title>/", show_experience_detail, name="show_experience_detail"),
+]
