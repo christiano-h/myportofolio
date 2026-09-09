f9fcc06a571d1f80fc41f8b58430fc9a49d62431
ano <christianoimannuel@gmail.com>
2026-09-09 14:33:32 +0700

    Menyelesaikan Tutorial 2 implementasi MVT pada projek tugas indiv idu (branch ini hanya digunakan untuk tutorial jadi tidak akan diimplementasikan ke main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Christiano H",
+        "npm": "2506615280",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Halo semuanya :D Selamat datang di website apapun ini terserah presepsi kalian, maaf"
+            "yah gak sebagus punya kalian. Makasih sudah ngestalk sampai sini (づ◡﹏◡)づ"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Christiano H",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
